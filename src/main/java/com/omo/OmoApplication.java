1f7921dccae8eddfb3e33a19f51c0bad7ec17366
unknown <zmzm3301>
2023-03-24 17:30:32 +0900

    2차

--- src/main/java/com/omo/OmoApplication.java
+++ src/main/java/com/omo/OmoApplication.java
@@ -2,7 +2,10 @@ package com.omo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableJpaAuditing
 @SpringBootApplication
@@ -10,6 +13,15 @@ public class OmoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OmoApplication.class, args);
+	}
+	
+	@Configuration
+	public class ReactRoutingConfiguration implements WebMvcConfigurer {
+
+	    @Override
+	    public void addViewControllers(ViewControllerRegistry registry) {
+	        registry.addViewController("/{spring:(?!api$).*$}/**").setViewName("forward:/index.html");
+	    }
 	}
 
 }
